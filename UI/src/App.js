a8487eff05edb68eb97b7c188ece9b159a8a8c3f
EnisThaqi <et64641@ubt-uni.net>
2024-04-26 14:59:08 +0200

    UI

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import TestComponent from './components/TestComponent';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Erza ma e mira React JS</h1>
-      <TestComponent name={"Enisi"} lastname="Thaqi"/>
     </div>
   );
 }
 
 export default App;
+//<TestComponent name={"Enisi"} lastname="Thaqi"/>
+//import TestComponent from './components/TestComponent';
