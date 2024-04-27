59114e09cf7d68e12c6437755c0d91757cb1014e
ErzaBlakajj <eb57603@ubt-uni.net>
2024-04-27 14:40:56 +0200

    commit
    sidebar

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Sidebar from './components/Sidebar';
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div className='container fluid' >
+      <div className='row'>
+        <div className='col-2'></div>
+      </div>
+      <Sidebar />
     </div>
   );
 }
