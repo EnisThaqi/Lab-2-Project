1173ce8f2708ed9ed4a85a1028f8db31445005d3
EnisThaqi <et64641@ubt-uni.net>
2024-04-27 20:30:15 +0200

    Commit
    Nav bar

--- UI/src/App.js
+++ UI/src/App.js
@@ -2,18 +2,24 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Sidebar from './components/Sidebar';
+import Home from './components/Home';
+import { useState } from "react";
 
 
 
 function App() {
+  const [toggle, setoggle] = useState(true)
+  const Toggle = () => {
+    setoggle(!toggle)
+  }
   return (
     <div className='bg-secondary min-vh-100'>
       <div className='row'>
-        <div className='col-2 bg-white vh-100'>
+       {toggle && <div className='col-2 bg-white vh-100'>
         <Sidebar />
-        </div>
-        <div className='col-auto'>
-
+        </div>}
+        <div className='col'>
+          <Home Toggle={Toggle}/>
         </div>
       </div>
     </div>
---
+++ UI/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+function Home({Toggle}){
+    return(
+        <div className='px-3'>
+            <Nav Toggle={Toggle}/>
+
+        </div>
+    )
+}
+
+export default Home
